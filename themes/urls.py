10b098661488c1a8ba805f03e4a1ed1f42187a76
Ruslan2015 <r.dokudaev@yandex.ru>
2016-04-08 15:41:59 +0200

    Работа со стилями

--- themes/urls.py
+++ themes/urls.py
@@ -5,10 +5,8 @@ from . import views
 
 
 urlpatterns = [
-    # ex: /themes/
-    url(r'^', views.index, name='themes'),
     # ex: /themes/order_number/
-    url(r'^order_number/', views.index, name='themes_order_number'),
+    url(r'order_number/', views.index, name='themes_order_number'),
     # ex: /themes/order_name/
-    url(r'^order_name/', views.order_name, name ='themes_order_name'),
+    url(r'order_name/', views.order_name, name ='themes_order_name'),
 ]
