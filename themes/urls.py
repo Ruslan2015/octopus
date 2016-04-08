2028621f97e52538a71e9f233797c3b11e59a331
Ruslan2015 <r.dokudaev@yandex.ru>
2016-04-08 08:51:05 +0200

    Разобрался с URL

--- themes/urls.py
+++ themes/urls.py
@@ -2,9 +2,13 @@ from django.conf.urls import url
 
 from . import views
 
-app_name = 'themes'
+
 
 urlpatterns = [
     # ex: /themes/
-    url(r'^$', views.index),
+    url(r'^', views.index, name='themes'),
+    # ex: /themes/order_number/
+    url(r'^order_number/', views.index, name='themes_order_number'),
+    # ex: /themes/order_name/
+    url(r'^order_name/', views.order_name, name ='themes_order_name'),
 ]
--- themes/views.py
+++ themes/views.py
@@ -5,6 +5,11 @@ from .models import Themes
 
 
 def index(request):
-    themes_list = Themes.objects.order_by('-number')
+    themes_list = Themes.objects.order_by('number')
+    context_dict = {'themes_list': themes_list}
+    return render(request, 'themes/index.html', context_dict)
+
+def order_name(request):
+    themes_list = Themes.objects.order_by('-name')
     context_dict = {'themes_list': themes_list}
     return render(request, 'themes/index.html', context_dict)
